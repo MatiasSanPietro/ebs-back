667fbec287dc146e5e338c4fdc61c1f8ba6f42b5
MatiasSanPietro <matisanpietro98@gmail.com>
2023-07-24 19:07:31 -0300

    carrito

--- src/routes/articulo.ts
+++ src/routes/articulo.ts
@@ -5,7 +5,6 @@ const router = Router();
 
 router.get("/api/articulos", articulo.getArticulos);
 router.get("/api/articulos/:id", articulo.getArticuloXID);
-
 router.post("/api/articulos/insert", articulo.insertArticulo);
 router.put("/api/articulos/update", articulo.actualizarArticulo);
 router.delete("/api/articulos/delete/:id", articulo.eliminarArticulo);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { Request, Response } from "express";
 import * as authController from "../controllers/AuthCtrl";
 
 const router = Router();
 
 router.post("/auth/register", authController.register);
-
 router.post("/auth/login", authController.login);
-
 router.post("/auth/logout", authController.logout);
 
 export default router;
